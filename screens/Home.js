3a1b1192a25d095b8a4d1cf022ad64b628c7ac6d
sxidsvit <s.p.antonyuk@gmail.com>
2021-04-03 21:13:26 +0300

    renderHeader()

--- screens/Home.js
+++ screens/Home.js
@@ -2,19 +2,86 @@ import React from 'react';
 import {
     View,
     Text,
-    TouchableOpacity
+    FlatList,
+    TouchableOpacity,
+    TouchableWithoutFeedbackBack,
+    Image,
+    ImageBackground,
+    Animated,
+    ScrollView,
+    SafeAreaView,
+    TouchableOpacityBase
 } from 'react-native';
+
+import { dummyData, SIZES, COLORS, FONTS, icons, image, images } from '../constants'
+
+
+
+const renderHeader = () => {
+    return (
+        <View style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            paddingHorizontal: SIZES.padding
+        }}>
+
+            {/* Profile */}
+            <TouchableOpacity
+                style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    width: 50,
+                    height: 50
+                }}
+                onPress={() => console.log('Profile')}
+            >
+
+                <Image
+                    source={images.profile_photo}
+                    style={{
+                        width: 40,
+                        height: 40,
+                        borderRadius: 20
+                    }}
+                />
+            </TouchableOpacity>
+
+            {/* ToucableOpacity */}
+            <TouchableOpacity
+                style={{
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: 50,
+                    height: 50
+                }}
+                onPress={() => console.log('Screen Mirror')}
+            >
+
+                <Image
+                    source={icons.airplay}
+                    style={{
+                        width: 25,
+                        height: 25,
+                        tintColor: COLORS.primary
+                    }}
+                />
+            </TouchableOpacity>
+
+        </View>
+    )
+}
 
 const Home = ({ navigation }) => {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home</Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate("MovieDetail")}
-            >
-                <Text>Navigate to MovieDetail</Text>
-            </TouchableOpacity>
-        </View>
+        <SafeAreaView
+            style={{
+                flex: 1,
+                backgroundColor: COLORS.black,
+            }}>
+            { renderHeader()}
+
+        </SafeAreaView>
     )
 }
 
