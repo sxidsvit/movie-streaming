b3004be68d51519acec8113cc290b39be7642508
sxidsvit <s.p.antonyuk@gmail.com>
2021-04-04 22:03:31 +0300

    Detail Category And Rating

--- screens/MovieDetail.js
+++ screens/MovieDetail.js
@@ -8,8 +8,9 @@ import {
     StyleSheet,
     ScrollView,
     Platform
-} from 'react-native';
+} from 'react-native'
 
+import LinearGradient from 'react-native-linear-gradient'
 import { ProgressBar } from '../components'
 import { COLORS, FONTS, SIZES, icons } from '../constants'
 
@@ -95,21 +96,129 @@ const MovieDetail = ({ navigation, route }) => {
                     flex: 1,
                 }}>
                     {renderHeaderBar()}
+                    <View style={{
+                        flex: 1,
+                        justifyContent: 'flex-end'
+                    }}>
+                        <LinearGradient
+                            start={{ x: 0, y: 0 }}
+                            end={{ x: 0, y: 1 }}
+                            colors={['transparent', '#000']}
+                            style={{
+                                width: '100%',
+                                height: 150,
+                                alignItems: 'center',
+                                justifyContent: 'flex-end'
+                            }}
+                        >
+                            {/* Season */}
+                            <Text style={{
+                                color: COLORS.white,
+                                ...FONTS.body4
+                            }}>
+                                {selectedMovie?.details?.season}
+                            </Text>
+
+                            {/* Name */}
+                            <Text style={{
+                                color: COLORS.white,
+                                ...FONTS.h1
+                            }}>
+                                {selectedMovie?.name}
+                            </Text>
+                        </LinearGradient>
+                    </View>
                 </View>
             </ImageBackground>
         )
     }
 
+    const renderCategoryAndRatings = () => {
+
+        return (
+            <View
+                style={{
+                    flexDirection: 'row',
+                    marginTop: SIZES.base,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}>
+                {/* Age  */}
+                <View
+                    style={[styles.categoryContainer,
+                    {
+                        marginLeft: 0
+                    }]}>
+                    <Text style={{
+                        color: COLORS.white,
+                        ...FONTS.h4
+                    }}
+                    >{selectedMovie?.details?.age}</Text>
+                </View>
+                {/* Genre */}
+                <View
+                    style={[styles.categoryContainer,
+                    {
+                        paddingHorizontal: SIZES.padding
+                    }]}>
+                    <Text style={{
+                        color: COLORS.white,
+                        ...FONTS.h4
+                    }}
+                    >{selectedMovie?.details?.genre}</Text>
+                </View>
+                {/* Ratings  */}
+                <View
+                    style={[styles.categoryContainer,
+                    {
+                        paddingHorizontal: SIZES.padding
+                    }]}>
+                    <Image
+                        source={icons.star}
+                        resizeMode="contain"
+                        style={{
+                            width: 15,
+                            height: 15,
+                            marginRight: 5
+                        }}
+
+                    />
+                    <Text style={{
+                        color: COLORS.white,
+                        ...FONTS.h4
+                    }}
+                    >{selectedMovie?.details?.ratings}</Text>
+                </View>
+            </View>
+        )
+    }
 
     return (
         <ScrollView
             contentContainerStyle={{ flex: 1, backgroundColor: COLORS.black }}
             style={{ backgroundColor: COLORS.black }}
         >
+            {/* Header  */}
             {renderHeaderSection()}
+            {/* Category & Rating  */}
+            {renderCategoryAndRatings()}
+            {/* Movie Details  */}
+
         </ScrollView>
 
     )
 }
 
+const styles = StyleSheet.create({
+    categoryContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: SIZES.base,
+        paddingHorizontal: SIZES.base,
+        paddingVertical: 3,
+        borderRadius: SIZES.base,
+        backgroundColor: COLORS.gray1
+    }
+})
 export default MovieDetail;
